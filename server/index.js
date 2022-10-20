5c6d652a7dfbc9cde27cd63e09c564c1d84a5ccc
nathanRenardC2 <nathan.renard@etu.univ-grenoble-alpes.fr>
2022-10-20 22:00:10 +0200

    method GET POST et PUT du model report configuré

---
+++ server/index.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const { PrismaClient } = require('@prisma/client');
+const bodyParser = require('body-parser')
+
+const prisma = new PrismaClient()
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+app.use(express.json());
+
+app.use(
+  (err, req, res, next) => {
+    console.error(err)
+    res.status(500)
+    res.json(err.message)
+  }
+);
+
+// method GET to get all reports
+app.get("/api/reports", async (req, res) => {
+  const reports = await prisma.report.findMany();
+  res.json(reports);
+}
+);
+
+// Method GET to get a report by id
+app.get("/api/reports/:id", async (req, res) => {
+  const report = await prisma.report.findUnique({
+    where: {
+      id: Number(req.params.id),
+    },
+  });
+  res.status(200)
+  res.json(report);
+});
+
+
+// method POST to create a report
+app.post("/api/reports", async (req, res) => {
+  console.log(req.body);
+  const report = await prisma.report.create({
+    data: {
+      title: req.body.title,
+      categorie: req.body.categorie,
+      money_depense: Number(req.body.money_depense),
+      createdAt: new Date(),
+    },
+  });
+  res.json(report);
+  res.status(201);
+});
+
+// method PUT to update a report
+app.put("/api/reports/:id", async (req, res) => {
+  const report = await prisma.report.update({
+    where: {
+      id: Number(req.params.id),
+    },
+    data: {
+      title: req.body.title,
+      categorie: req.body.categorie,
+      money_depense: Number(req.body.money_depense),
+    },
+  });
+  res.json(report);
+  res.status(200);
+}
+);
+
+
+const port = process.env.PORT || "3000";
+
+const server = app.listen(port)
---
+++ server/prisma/migrations/20221020193527_/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Report" ALTER COLUMN "money_depense" SET DATA TYPE DOUBLE PRECISION;
